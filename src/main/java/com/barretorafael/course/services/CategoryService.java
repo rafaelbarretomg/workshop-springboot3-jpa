c586b448e7c6c6ae2d911439e7aaa44ade987fbb
Rafael Barreto <rafaelbarretomg@gmail.com>
2024-02-10 01:46:47 -0300

    Category

---
+++ src/main/java/com/barretorafael/course/services/CategoryService.java
@@ -0,0 +1,27 @@
+package com.barretorafael.course.services;
+
+import com.barretorafael.course.entities.Category;
+import com.barretorafael.course.entities.User;
+import com.barretorafael.course.repositories.CategoryRepository;
+import com.barretorafael.course.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class CategoryService {
+
+    @Autowired
+    private CategoryRepository repository;
+
+    public List<Category> findAll() {
+        return repository.findAll();
+    }
+
+    public Category findById(Long id) {
+        Optional<Category> obj = repository.findById(id);
+        return obj.get();
+    }
+}
